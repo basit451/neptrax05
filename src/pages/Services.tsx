6970ada469351ea71102fd197bc97e1d70e28bb5
basit451 <136922439+basit451@users.noreply.github.com>
2025-11-13 18:27:09 +0000

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -175,11 +175,11 @@ export default function Services({ onNavigate }: ServicesProps) {
           {/* UNCOMMENT ONE OPTION AT A TIME TO TEST */}
 
           {/* Option 1: Floating Geometric Shapes */}
-           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div className="w-20 h-20 bg-gradient-to-r from-[#3b82f6] to-[#60a5fa] rounded-lg animate-float-slow opacity-30 rotate-45"></div>
             <div className="w-16 h-16 bg-gradient-to-r from-[#1e3a8a] to-[#2563eb] rounded-full animate-float-medium opacity-40 absolute top-1/4 left-1/4 animation-delay-1000"></div>
             <div className="w-24 h-24 bg-gradient-to-r from-[#60a5fa] to-[#93c5fd] rounded-lg animate-float-fast opacity-25 absolute bottom-1/4 right-1/4 animation-delay-2000 rotate-12"></div>
-          </div>
+          </div> */}
 
           {/* Option 2: Morphing Blob */}
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -187,12 +187,12 @@ export default function Services({ onNavigate }: ServicesProps) {
           </div>
 
           {/* Option 3: Sparkle Field */}
-          {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div className="w-2 h-2 bg-white rounded-full animate-sparkle opacity-60"></div>
             <div className="w-1 h-1 bg-[#60a5fa] rounded-full animate-sparkle opacity-80 animation-delay-200 absolute top-10 left-20"></div>
             <div className="w-1 h-1 bg-[#3b82f6] rounded-full animate-sparkle opacity-70 animation-delay-400 absolute bottom-10 right-20"></div>
             <div className="w-1 h-1 bg-[#93c5fd] rounded-full animate-sparkle opacity-90 animation-delay-600 absolute top-20 right-10"></div>
-          </div> */}
+          </div>
 
           {/* Option 4: Floating Orbs Cluster */}
           {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
